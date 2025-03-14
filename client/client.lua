2fbd48b48991fc6ada60c5b5ad1ece426e286e57
Snowly <visionhyronic@gmail.com>
2025-03-14 22:43:51 +0100

    add "duty" to toggle duty.

--- client/client.lua
+++ client/client.lua
@@ -15,22 +15,43 @@ function ExitJobZone(job)
     if not onDuty then return end
     TriggerServerEvent('QBCore:ToggleDuty')
 end
-
+local state = false
 CreateThread(function()
     for _, zone in pairs(Config.Zones) do
-        zones[zone.job] = lib.zones.poly({
+        zones[zone.job] = {}
+        zones[zone.job].zone = lib.zones.poly({
             points = zone.zone.points,
             thickness = zone.zone.thickness,
             debug = Config.Debug,
-            onExit = function() ExitJobZone(zone.job) end,
+            onExit = function() ExitJobZone(zone.job) end
         })
+        local point = lib.points.new({
+            coords = zone.duty.coords,
+            distance = zone.duty.distance,
+        })
+        function point:nearby()
+            DrawMarker(zone.duty.marker.type, self.coords.x, self.coords.y, self.coords.z, 0.0, 0.0, 0.0, 0.0, 180.0, 0.0, 1.0, 1.0, 1.0, zone.duty.marker.red, zone.duty.marker.green, zone.duty.marker.blue, zone.duty.marker.opacity, false, true, 2, false, nil, nil, false)
+            if self.currentDistance < 1.5 then
+                if not lib.isTextUIOpen() then
+                    lib.showTextUI('[E] Toggle Duty')
+                end
+                if IsControlJustReleased(0, 38) then
+                    TriggerServerEvent('QBCore:ToggleDuty')
+                end
+            end
+            if self.currentDistance > 1.5 then
+                lib.hideTextUI()
+            end
+        end
+        zones[zone.job].duty = point
     end
 end)
 
 AddEventHandler('onResourceStop', function(resource)
     if resource == GetCurrentResourceName() then
         for _, zone in pairs(zones) do
-            zone:remove()
+            zone.zone:remove()
+            zone.duty:remove()
         end
     end
 end)
--- config.lua
+++ config.lua
@@ -12,6 +12,18 @@ return {
                 },
                 thickness = 4.0,
             },
-        }
+            duty = {
+                coords = vec3(370.6229, -612.3512, 28.8611),
+                distance = 3.0,
+                marker = {
+                    type = 2,
+                    red = 255,
+                    green = 0,
+                    blue = 0,
+                    opacity = 50,
+                }
+            }
+    
+        },
     }
 }
